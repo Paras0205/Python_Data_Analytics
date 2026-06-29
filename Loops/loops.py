64f896c6c351656a8d1755f3cd9838704757af95
Paras <parasdhankhar02@gmail.com>
2026-06-29 21:39:18 +0530

    Add files via upload

---
+++ Loops/loops.py
@@ -0,0 +1,98 @@
+# A loop repeats work so you don’t have to write the same code again and again.
+# For example, instead of printing numbers 1 to 5 manually, you can use a loop.
+
+# Example:
+
+for i in range(1, 6):
+    print(i)
+
+# for loop
+# Use a for loop when you know how many times you want to repeat something,
+# or when you want to go through items in a sequence like a list, string, or range
+
+for i in range(5):
+    print(i)
+
+# while loop
+# Use a while loop when you want repetition to continue until a condition becomes false.
+
+# Example:
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1    
+
+# break
+# break is used to stop a loop early when a condition is met.
+
+# Example:
+
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+
+# continue
+# continue skips the current iteration and moves to the next one.
+
+# Example:
+
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+
+# Nested loops
+# A nested loop means one loop inside another.
+
+# Example:
+
+for i in range(1, 4):
+    for j in range(1, 3):
+        print(i, j)
+
+
+# Imp loops pattern
+
+# 1.Accumulator Pattern (SUM / TOTAL)
+# Used when:
+#     You need total
+#     You need combined value
+
+# total = 0
+# for x in data:
+#     total += x   
+    
+# Key idea:
+#     Variable outside loop, update inside loop
+
+
+# 2.Counter Pattern (COUNT)
+# Used when:
+#     Counting items that match a condition
+
+# count = 0
+# for x in data:
+#     if x > 100:
+#         count += 1
+        
+
+# 3.Filter Pattern (SELECT)
+# Used when:
+#     Printing or storing specific items
+
+# for x in data:
+#     if x > 100:
+#         print(x)
+
+
+# 4.Search Pattern (FIND)
+# Used when:
+#     Checking if something exists
+
+# found = False
+# for x in data:
+#     if x == target:
+#         found = True
